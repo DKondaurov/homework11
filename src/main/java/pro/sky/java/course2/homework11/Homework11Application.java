20427b0ca7866ad3466603462bfb4bb3c570ea35
homework <9874@bk.ru>
2022-02-13 17:17:13 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/homework11/Homework11Application.java
@@ -0,0 +1,14 @@
+package pro.sky.java.course2.homework11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class Homework11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework11Application.class, args);
+    }
+
+}
